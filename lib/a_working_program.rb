70d529efbd25635be4af118eaf886561b828c639
Jason  Montes <jasone.montes@gmail.com>
2019-04-10 04:01:07 +0000

    Automatically backed up by Learn

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -1,8 +1,12 @@
+<<<<<<< HEAD
 
 puts "Hello World!"
  # Correct this to "Hello World!"
 =======
 puts "Hello World!" # Correct this to "Hello World!
+=======
+puts "Hello World!" # Correct this to "Hello World!"
+>>>>>>> 1b871b3cb97ddbc76b22db3001d39f0f256f584c
 
 # Your entire program should read:
 # puts "Hello World!"
